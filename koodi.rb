0adc8195f602bde4264a43510c6195db1832dad0
villetan <ville.tanskanen@helsinki.fi>
2016-01-15 23:32:34 +0200

    tehtava9 done

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,14 @@
+
 # kirjoita koodi tänne
+def esiintymat(taulukko)
+  esiintyneet = Hash.new
+  for i in taulukko
+    if !esiintyneet.include? i
+      esiintyneet[i]=1
+    else
+      esiintyneet[i]=esiintyneet[i] + 1
+      end
+  end
+  esiintyneet
+end
+
