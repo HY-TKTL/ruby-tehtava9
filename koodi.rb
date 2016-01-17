edb9bb7e505cadc1461478c459c0dfbf52b3ecb6
NRoos <nroos@dhcp-asv-101.helsinki.fi>
2016-01-17 09:22:37 +0200

    getting *0! done

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,14 @@
 # kirjoita koodi tänne
+
+def esiintymat(a_table)
+  n = a_table.size()
+  hash = {}
+
+  n.times do |i|
+    temp = a_table.at(i)
+    hash[temp] = 0 if hash[temp].nil?
+    hash[temp] += 1
+  end
+
+  return hash
+end
