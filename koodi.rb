6610658f6b8be78f026598e5447ad3b100dd803c
totutotu <totutotu@cs.helsinki.fi>
2016-01-17 14:09:21 +0200

    tehty

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,9 @@
-# kirjoita koodi tänne
+def esiintymat(taulu)
+	h = Hash.new
+	taulu.length.times do |i|
+		h[taulu.at(i)] = 0 if h.assoc(taulu.at(i)) == nil
+		h[taulu.at(i)] = h[taulu.at(i)] + 1
+		end
+	return h
+end
+		
