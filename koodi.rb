e7053ff4e3db7708240eba674e22419947f67fe8
Iina Lumme <iilumme@cs.helsinki.fi>
2016-01-19 22:36:07 +0200

    done

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,17 @@
 # kirjoita koodi tänne
+def esiintymat(a)
+  hash = {}
+
+  a.each do |i|
+
+    if hash.has_key? i
+      hash[i] = hash[i] + 1
+    else
+      hash[i] = 1
+    end
+
+  end
+
+  hash
+
+end
