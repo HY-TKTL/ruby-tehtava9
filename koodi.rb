618acb64f7c4f13dcf94aa68599a3e407119ffff
banjohirvi <ollilari@gmail.com>
2016-01-19 23:42:06 +0200

    Tehtava 9

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,13 @@
 # kirjoita koodi tänne
+def esiintymat(a)
+  b = {}
+  a.each do |alkio|
+    c = b[alkio];
+    if !c
+      b[alkio] = 1
+    else
+      b[alkio] = c + 1
+    end
+  end
+  return b
+end
