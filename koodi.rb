197c941628773b5debda1aef32eafbad2b28f74d
Juho <juho.kyyhkynen@helsinki.fi>
2016-01-20 10:33:14 +0200

    t9

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,14 @@
+def esiintymat taulu
+  hash = {}
+  taulu.each do |alkio|
+    if hash.has_key?(alkio)
+      hash[alkio] = hash[alkio]+1
+    else
+      hash[alkio] = 1
+    end
+  end
+  hash
+end
 # kirjoita koodi tänne
+haj = esiintymat [1, 2, 1, 1, 3, 2]
+puts haj.to_s
