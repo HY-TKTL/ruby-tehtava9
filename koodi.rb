99e21f51fd41e8e1ba67cbc48f505b7c2aa4a9ee
Satu Asikainen <satu.asikainen@helsinki.fi>
2016-01-23 21:04:42 +0200

    tehtävä 9 palautus 1

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,12 @@
 # kirjoita koodi tänne
+def esiintymat(taulu)
+  hash = {}
+  a = taulu.length - 1
+  for i  in  0..a
+    taulu2 =[]
+    taulu2 = taulu.select{|x| x == taulu[i]}
+    hash[taulu[i]]=taulu2.length
+  end
+return hash
+end
+
