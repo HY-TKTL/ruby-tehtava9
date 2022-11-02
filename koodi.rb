ff0b3f9129d6f3f7aebb95795306f964adb3fb11
Hannu Kulmala <hanski22@pm.me>
2022-11-02 12:16:44 +0200

    Tehtava9

--- koodi.rb
+++ koodi.rb
@@ -1,1 +1,10 @@
-# kirjoita koodi tänne
+def esiintymat(t)
+    hsh = {}
+    t.each do |item|
+        hsh[item] = 0 if not hsh.include?(item)
+        hsh[item] += 1
+    end
+    return hsh
+end
+
+
